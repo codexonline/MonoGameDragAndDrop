df1031e7eb93312f35bc982a3d4cf1b9bc498ce2
codexonline <bjxxi@hotmail.com>
2018-07-23 14:43:14 -0400

    real initial commit

---
+++ Sprites/SpriteAnimator.cs
@@ -0,0 +1,70 @@
+﻿/* Attribution (a) 2016 The Ruge Project (http://ruge.metasmug.com/) 
+ * Unlicensed under NWO-CS (see UNLICENSE)
+ */
+
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using System.Collections.Generic;
+
+namespace MonoGameDragAndDrop.Sprites {
+
+    class SpriteAnimator {
+
+        SpriteGrid spriteGrid;
+        List<Vector2> queue;
+        Vector2 idle;
+        int playIndex = 0;
+        bool loop;
+
+        public SpriteAnimator(Vector2 idle, SpriteGrid spriteGrid, bool loop = true) {
+
+            this.idle = idle;
+            this.spriteGrid = spriteGrid;
+            this.loop = loop;
+            queue = new List<Vector2>();
+
+        }
+
+        public void Add(Vector2 vector) { queue.Add(vector); }
+
+        public void Clear() { queue.Clear();  }
+
+
+        public Rectangle play() {
+
+            Rectangle rect = spriteGrid.getRectangle(idle);
+
+            if (queue.Count > 0) { 
+
+                // check if it is out of range
+                if (playIndex == queue.Count) {
+
+                    if (!loop) { 
+                        queue.Clear();
+                        return rect;
+                    }
+                    else {
+                        playIndex = 0;
+                    }
+
+                }
+            
+                Vector2 current = queue[playIndex];
+                rect = spriteGrid.getRectangle(current);
+            
+                playIndex++;
+            }
+
+            return rect;
+
+        }
+
+        public Rectangle stop() {
+
+            return spriteGrid.getRectangle(idle);
+
+        }
+        
+    }
+
+}
---
+++ Sprites/SpriteGrid.cs
@@ -0,0 +1,50 @@
+﻿/* Attribution (a) 2016 The Ruge Project (http://ruge.metasmug.com/) 
+ * Unlicensed under NWO-CS (see UNLICENSE)
+ */
+
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace MonoGameDragAndDrop.Sprites {
+
+    class SpriteGrid {
+
+        private int rows, cols, width, height;
+        private Vector2 offset;
+
+        public SpriteGrid(int rows, int cols, int width, int height, Vector2 offset) {
+
+            this.rows = rows;
+            this.cols = cols;
+            this.width = width;
+            this.height = height;
+            this.offset = offset;
+
+        }
+
+        public SpriteGrid(int rows, int cols, int width, int height) : this(rows, cols, width, height, Vector2.Zero) {}
+
+            
+        public Rectangle getRectangle(Vector2 vector) {
+
+            int x = (int)vector.X * width;
+            int y = (int)vector.Y * height;
+
+            x += (int)offset.X;
+            y += (int)offset.Y;
+
+            Rectangle rect = new Rectangle(x, y, width, height);
+            return rect;
+
+        }
+
+        public Rectangle getRectangle(int col, int row) {
+
+            Vector2 vect = new Vector2(col, row);
+            Rectangle rect = getRectangle(vect);
+            return rect;
+
+        }
+
+    }
+}
